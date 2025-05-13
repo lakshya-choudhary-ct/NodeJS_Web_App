181c443562ec42e85cbcb94658cd1eea9dc7b6ba
Lakshya Choudhary <lakshya.choudhary@celebaltech.com>
2025-05-13 17:18:31 +0530

    First

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+    res.send("Hello Express And Azure");
+})
+
+app.listen(port, function(req, res) {
+    console.log(`Server Up And Running On Port ${port}`);
+})
