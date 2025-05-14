2c0c2639b598ef13aa889581e1f175ca17a7e4e9
lakshya-choudhary-ct <lakshya.choudhary@celebaltech.com>
2025-05-14 16:11:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
     res.send("Hello Express And Azure DevOps");
 })
 
-app.listen(port, function(req, res) {
-    console.log(`Server Up And Running On Port ${port}`);
+app.listen(PORT, function(req, res) {
+    console.log(`Server Up And Running On Port ${PORT}`);
 })
